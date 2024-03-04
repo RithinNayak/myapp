5a802285736d14f3b5df454206180fbe1a2d1c6a
kundinsai43 <kundinasai43@gmail.com>
2024-03-04 15:11:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Header'
+
+function App() {
+  return (
+<div >
+ <div> <h1><Header/></h1></div>
+ </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+ function Header() {
+  return (
+    <>
+  <div>Header</div>
+  <p>This is a paragraph</p>
+  </>)
+}
+export default Header
