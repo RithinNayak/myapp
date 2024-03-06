a815b9e381dae048bdc1091b14ffc054cfd8b679
kundinsai43 <kundinasai43@gmail.com>
2024-03-06 15:47:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-import Header from './Header'
-
+import "./App.css";
+import Calc from "./Calc";
+import Add from "./Add";
+import Students from "./Students";
+import Sqr from "./Sqr";
+import User from "./User";
 function App() {
+  const sqrfunction = (n) => {
+    return n*n
+  }
   return (
-<div >
- <div> <h1><Header/></h1></div>
- </div>
+    <div>
+      <User name="John"/>
+     <Calc a={3} b={5}/>
+     <Add arr={[5,6,8]} />
+     <Students score={{'John':40,'Cathy':50}}/>
+     <Sqr a={5} fn={sqrfunction}/>
+    </div>
   );
 }
-
 export default App;
---
+++ src/App1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function App1(props) {
+  return (
+    <div>Hello {props.name}. You are {props.age}</div>
+  )
+}
+App1.defaultProps={name:"Cathy",age:20}
