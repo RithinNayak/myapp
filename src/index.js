ad50736b537eb6098b3ed90513f0ecb22c180a7e
kundinsai43 <kundinasai43@gmail.com>
2024-03-09 16:15:17 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App7 from "./App7";
+import App6 from "./App6";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <App7/>
+    <App6/>
   // </React.StrictMode>
 );
